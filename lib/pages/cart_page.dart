a56d74604810a3cf040ec719601d3438ac002c1d
John <85222720+johnomeyo@users.noreply.github.com>
2023-11-29 13:39:01 +0300

    remove from cart by sliding

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,16 @@
 // import 'dart:js';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:local_sell/components/beta.dart';
 import 'package:local_sell/components/constants.dart';
 import 'package:local_sell/providers/cart_provider.dart';
 import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
-  const CartPage({super.key});
+  const CartPage({
+    super.key,
+  });
 
   @override
   State<CartPage> createState() => _CartPageState();
@@ -34,17 +37,44 @@ class _CartPageState extends State<CartPage> {
           ),
         ),
         body: Consumer<CartModel>(
-          builder: (context, cart, child) => ListView.builder(
-              itemCount: cart.products.length,
-              itemBuilder: (context, index) {
-                final product = cart.products[index];
-                return CartTiles(
-                  name: product.name,
-                  category: product.size,
-                  price: product.price,
-                  imageUrl: product.imageUrl,
-                );
-              }),
+          builder: (context, cart, child) => cart.products.isEmpty
+              ? const Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.shopping_cart_outlined,
+                        size: 80,
+                      ),
+                      SizedBox(height: 10,),
+                      Text("Oops, your cart seems to be empty😪", style: TextStyle(fontSize: 17),),
+                    ],
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: cart.products.length,
+                  itemBuilder: (context, index) {
+                    final product = cart.products[index];
+                    return Slidable(
+                      endActionPane:
+                          ActionPane(motion: const StretchMotion(), children: [
+                        SlidableAction(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.red,
+                            icon: Icons.delete,
+                            label: 'Delete',
+                            onPressed: (BuildContext context) {
+                              cart.removeFromCart(product);
+                            })
+                      ]),
+                      child: CartTiles(
+                        name: product.name,
+                        category: product.size,
+                        price: product.price,
+                        imageUrl: product.imageUrl,
+                      ),
+                    );
+                  }),
         ));
   }
 }
